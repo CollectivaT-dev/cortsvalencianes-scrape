020eaf5f86e7644d16d04f2d45da9e91ad6d8975
gullabi <bkulebi@gmail.com>
2021-08-30 11:21:48 +0200

    metadata scrape added

--- scrape_corts.py
+++ scrape_corts.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup as bs4
 import requests
 import os
 import json
+import re
+
+RE_CL = re.compile('(,|\s+)')
 
 def main():
     query_url = 'https://mediateca.cortsvalencianes.es/library/items?page={}&action=index&controller=library%2Fitems&date=&date_from=&date_to=&gbody_id=&legs_id=&search_type=1&speaker_id=&text=&utf8=%E2%9C%93&view_type=list'
@@ -10,7 +13,7 @@ def main():
     localizer_pages = get_localizer_pages(query_url, max_page)
 
     localizer_dict = {}
-    for i, page in enumerate(localizer_pages[:2]):
+    for i, page in enumerate(localizer_pages):
         localizer_dict[i] = extract_localizer_page(page)
 
     with open('items.json', 'w') as out:
@@ -38,10 +41,21 @@ def extract_localizer_page(url):
         div_details = localizer.find('div', attrs={'class':'details'})
         #gallery_items_debug.append((a_element, div_details))
         gallery_item['url'] = a_element['href']
-        gallery_item['title'] = a_element.text
+        gallery_item['title'] = RE_CL.sub(' ', a_element.text).strip()
         # TODO parse div_details metadata
+        div_details = localizer.find('div', attrs={'class':'details'})
+        extract_div_details(gallery_item, div_details)
         gallery_items.append(gallery_item)
     return gallery_items
+
+def extract_div_details(gallery_item, div_details):
+    div = div_details.findChild()
+    div_cl = RE_CL.sub(' ', str(div))
+    keys = [result[1] for result in re.findall('(<span>)(.*?)(</span)', div_cl)]
+    values = [result[1] for result in re.findall('(</span>)(.*?)(<)', div_cl)]
+    for key, value in zip(keys, values):
+        gallery_item[key.strip()] = value.strip()
+    
 
 if __name__ == "__main__":
     main()
