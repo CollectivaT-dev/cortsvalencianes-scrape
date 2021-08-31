c5d01668dc09f8a9095164bc393045b51e732f71
gullabi <bkulebi@gmail.com>
2021-08-31 10:55:51 +0000

    js parsing working version

--- download.py
+++ download.py
@@ -25,7 +25,7 @@ class Stream(object):
         restore = PickleContextRestore()
         context = M3u8Context(file_url=self.chunklist_url,
                               referer='',
-                              threads=3,
+                              threads=5,
                               output_file=self.full_stream_path,
                               get_m3u8file_complete=False,
                               downloaded_ts_urls=[],
@@ -60,7 +60,7 @@ class Stream(object):
 
         chunk_code = re.sub('(chunklist_)|(\.m3u8)', '', self.chunklist)
         self.stream_filename = '_'.join([self.base_name, chunk_code])+'.ts'
-        page = self.base_name.strip('_')[0]
+        page = self.base_name.split('_')[0]
         # TODO add absolute path
         stream_path = os.path.join('sessions', page)
         self.full_stream_path = os.path.join(stream_path, self.stream_filename)
@@ -71,7 +71,7 @@ class Stream(object):
     def get_pointer_url(self):
         # TODO parse JS and get all parts
         res = requests.get(self.script_url, verify=False)
-        search = re.search('JSPLAYLIST.*m3u8',str(res.content))
+        search = re.search('JSPLAYLIST.*m3u8.*?;',str(res.content))
         if search:
             script = search.group()
         else:
@@ -80,28 +80,34 @@ class Stream(object):
 
         # parse JS
         playlist_vars = list(set(re.findall('JSPLAYLIST1\[\d\]', script)))
-        if len(playlist_vars) == 0
+        if len(playlist_vars) == 0:
             msg = 'stream pointer urls not found in script'
             raise ValueError(msg)
         for i in range(len(playlist_vars)):
-            elements = re.findall('JSPLAYLIST1\[%i].*?;'%i, script)
+            elements = re.findall('JSPLAYLIST1\[%i\].*?;'%(i+1), script)
+            if len(elements) == 0:
+                msg = 'stream pointer elements not found'
+                raise ValueError(msg)
             # TODO parse all elements
             for element in elements:
                 if 'm3u8' in element:
-                    search = re.search('http.*?m3u8', script)
+                    search = re.search('http.*?m3u8', element)
                     if search:
                         self.pointer_urls.append(search.group())
                     else:
                         msg = 'stream pointer url not found for element %i'%i
                         raise ValueError(msg)
+                    break
 
 def download_stream(recording, name):
     stream = Stream(recording['url'], name)
+    #stream.get_pointer_url()
     stream.download_stream()
 
 def main():
     items = json.load(open('items.json'))
     for page, recordings in items.items():
+    #for page, recordings in list(items.items())[-2:-1]:
         for i, recording in enumerate(recordings):
             if 'Ple' in recording['title']:
                 if not recording.get('uri'):
